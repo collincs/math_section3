57b6f25f9eea6122ed93d23e5c7fbc10dbb6f5b9
collincs <collincs@dukes.jmu.edu>
2016-04-30 12:52:16 -0400

    Added files via upload

---
+++ math_section3/spec/math_spec.js
@@ -0,0 +1,17 @@
+// tell jasmine where to find our code
+var math = require('../math'); 
+
+describe("A program that does basic arithmetic", function() {
+	it("can add two numbers together", function() {
+		expect(math.add(3,5)) .toBE(8);
+	};
+	it("can subtract two numbers together", function() {
+		expect(math.subtract(3,5)) .toBE(-2);
+	};
+	it("can multiply two numbers together", function() {
+		expect(math.multiply(3,5)) .toBE(15);
+	};
+	it("can divide two numbers together", function() {
+		expect(math.divide(6,2)) .toBE(3);
+	})
+});
